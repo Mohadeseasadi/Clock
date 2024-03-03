3c8603ea528e270476d080e5c1085f3281d9efc8
mohadese asadi <mohiw.1380@gmail.com>
2024-03-03 14:51:47 +0330

    create Data

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ function showtime(){
     // get element htm
     var myClock = document.getElementById("my-clock");
 
+    // create data method
+    var date = new Date();
+    var hours = date.getHours();
+    var minuts = date.getMinutes();
+    var secounds = date.getSeconds();
+    var session = "AM"
+
     
 
 
